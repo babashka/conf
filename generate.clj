da8a1727e8445b63a6c616e29cf748f208e0af76
Michiel Borkent <michielborkent@gmail.com>
2025-12-04 09:53:47 +0100

    website

--- generate.clj
+++ generate.clj
@@ -83,7 +83,7 @@
    [:p.text-xl.mt-4 "Make sure to get your ticket at "[:a {:href "https://www.meetup.com/the-dutch-clojure-meetup/events/312079164/"} "meetup.com"] " while they are still available."]
    [:p.text-xl.mt-4 "Babashka-conf is organized the day before " [:a {:href "https://clojuredays.org/"} "Dutch Clojure Days 2026"] " so you can have two days of Clojure fun in and around Amsterdam!"]
    [:p.text-xl.mt-4 "Keep an eye on this website as it will up be updated in the coming months. CfP starts mid January when the CfP of DCD26 closes."]
-   [:p.text-xl.mt-4 "In case of questions, you can reach us at " [:a {:href "mailto:babashka.conf@gmail.com"} "babashka.conf@gmail.com."] "or join the " [:a {:href "https://app.slack.com/client/T03RZGPFR/C04VAK5U86L"} "Slack channel."]]
+   [:p.text-xl.mt-4 "In case of questions, you can reach us at " [:a {:href "mailto:babashka.conf@gmail.com"} "babashka.conf@gmail.com."] " and in the " [:a {:href "https://app.slack.com/client/T03RZGPFR/C04VAK5U86L"} "#babashka-conf Slack channel."]]
    [:p.text-xl.mt-4 "We are looking for sponsors. More information about this will be posted on this website, but if you are eager to sponsor, you can already reach out to us."]
    [:p.text-xl.mt-4 "You can expect some community-organized activities before and after the conference, information about this will be posted in the Slack channel."]])
 
