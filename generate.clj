336367506a53b57e3f8936021da7c8d27e0596e4
Michiel Borkent <michielborkent@gmail.com>
2025-12-04 09:50:02 +0100

    wording

--- generate.clj
+++ generate.clj
@@ -84,7 +84,8 @@
    [:p.text-xl.mt-4 "Babashka-conf is organized the day before " [:a {:href "https://clojuredays.org/"} "Dutch Clojure Days 2026"] " so you can have two days of Clojure fun in and around Amsterdam!"]
    [:p.text-xl.mt-4 "Keep an eye on this website as it will up be updated in the coming months." "CfP starts mid January when the CfP of DCD26 closes."]
    [:p.text-xl.mt-4 "In case of questions, you can reach us at " [:a {:href "mailto:babashka.conf@gmail.com"} "babashka.conf@gmail.com."] "or join the " [:a {:href "https://app.slack.com/client/T03RZGPFR/C04VAK5U86L"} "Slack channel."]]
-   [:p.text-xl.mt-4 "We are looking for sponsors. More information about this will be posted on this website, but if you are eager to sponsor, you can already reach out to us."]])
+   [:p.text-xl.mt-4 "We are looking for sponsors. More information about this will be posted on this website, but if you are eager to sponsor, you can already reach out to us."]
+   [:p.text-xl.mt-4 "You can expect some community-organized activities before and after the conference, information about this will be posted in the Slack channel."]])
 
 #_(def announcements
   [:section
@@ -100,7 +101,7 @@
     [:section
      [:h2.text-6xl.text-center {:style "font-family: Forum, serif;"} "Babashka-conf returns, this time in Amsterdam NL!"]
      [:br]
-     [:p.text-4xl.text-center {:style "font-family: Forum, serif;"} "8th of May 2026, 13:00-17:00 with extra community organized activities before and after."]
+     [:p.text-4xl.text-center {:style "font-family: Forum, serif;"} "8th of May 2026, 13:00-17:00, OBA Oosterdok, Amsterdam (OBA Congres, 6th floor)"]
      intro]
 
     #_(hr)
